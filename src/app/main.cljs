3323ad0cd4ea32034defeaaafeaf05a3c1a7aad0
Waldemar <waldemar.schwan@uswitch.com>
2019-04-09 20:34:35 +0100

    WIP

--- src/app/main.cljs
+++ src/app/main.cljs
@@ -1,16 +1,37 @@
 (ns app.main
   (:require [reagent.core :as r]))
 
+(defonce active-view-id (r/atom :home))
+
+(defn <home>
+  []
+  [:div
+   [:h1 "home view"]
+   [:button {:on-click #(do (prn ::new-session)
+                            (reset! active-view-id :session-new))}
+    "new session"]
+   [:button "join session"]])
+
+(defn <session-new>
+  []
+  [:h2 "Sesson New"])
+
+(def views {:home        <home>
+            :session-new <session-new>})
+
 (defn app
   []
-  [:h1 "DND Mapper"])
+  [:div
+   [:h1 "DND Mapper"]
+   (let [active-view (get views @active-view-id)]
+     [active-view])])
 
-(defn reload!
+(defn ^:dev/after-load render
   []
-  (prn ::reload!))
+  (prn ::render)
+  (r/render [app] (js/document.getElementById "app")))
 
-(defn ^:export  main!
+(defn ^:export  main
   []
-  (prn ::main!)
-  (r/render [app]
-            (js/document.getElementById "app")))
+  (prn ::main)
+  (render))
