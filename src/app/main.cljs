77a6ea581a83051f29b75dd681cb0527bcd61224
Waldemar <waldemar.schwan@uswitch.com>
2019-04-10 22:51:56 +0100

    working example

--- src/app/main.cljs
+++ src/app/main.cljs
@@ -3,6 +3,11 @@
 
 (defonce active-view-id (r/atom ::home))
 
+(defonce highlight-overlay (r/atom false))
+(defonce map-width  (r/atom 35))
+(defonce map-height (r/atom 50))
+
+
 (defn <nav>
   []
   [:nav
@@ -18,7 +23,6 @@
    [:button {:on-click #(reset! active-view-id ::session-new)}
     "new session"]
    [:button "join session"]])
-
 
 
 (defn <map-definition-input>
@@ -41,30 +45,39 @@
        :min 1
        :value @map-height
        :on-change #(reset! map-height (some-> % .-target .-value int))
-       }]]))
+       }]
+
+     [:label {:for "#map-height"} "highlight overlay"]
+     [:input#highlight-overlay
+      {:type :checkbox
+       :checked @highlight-overlay
+       :on-change #(reset! highlight-overlay (some-> % .-target .-checked))
+       }]
+     ]))
 
 (defn <map-preview>
   [w h active-position]
   (fn []
-    [:div
+    [:div.map-preview-wrapper
      [:img.map-preview__map {:src "https://img00.deviantart.net/d36a/i/2015/115/3/0/abandoned_temple_of_blackfire_by_dlimedia-d4pponv.jpg"
             :alt "Created by DLIMedia: https://www.deviantart.com/dlimedia/art/Abandoned-Temple-of-Blackfire-285053467"}]
      [:table.map-preview
       [:tbody.map-preview__tbody
        (doall
          (for [y (range @h)]
-           [:tr {:key (str "m-prev-y" y)}
+           [:tr.map-preview-row {:key (str "m-prev-y" y)}
             (doall
               (for [x (range @w)]
-                [:td.map-cell {:key (str "map-prev-yx-" y x)
-                               :on-click #(reset! active-position [x y])
-                               :class (if (= [x y] @active-position)
-                                        "map-cell__active"
-                                        "")}
-                 (str "(" x "," y ")")]))]))]]]))
+                [:td.map-preview-cell
+                 {:key (str "map-prev-yx-" y x)
+                  :on-click #(reset! active-position [x y])
+                  :class [(when (= [x y] @active-position)
+                           "map-cell__active")
+                          (when @highlight-overlay
+                            "map-cell__highlight")]}
+                 ;(str "(" x "," y ")")
+                 ]))]))]]]))
 
-(defonce map-width  (r/atom 5))
-(defonce map-height (r/atom 5))
 
 (defn <session-new>
   []
