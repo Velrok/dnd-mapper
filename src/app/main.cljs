15f2afed8d81b89ddfda0dc981935b8532e7740a
Waldemar <waldemar.schwan@uswitch.com>
2019-04-12 22:40:41 +0200

    wip

--- src/app/main.cljs
+++ src/app/main.cljs
@@ -7,6 +7,17 @@
 (defonce map-width  (r/atom 35))
 (defonce map-height (r/atom 50))
 
+(def players
+  ["https://media-waterdeep.cursecdn.com/avatars/thumbnails/4729/162/150/300/636756769380492799.png"
+   "https://media-waterdeep.cursecdn.com/avatars/thumbnails/17/747/150/150/636378331895705713.jpeg"
+   "https://media-waterdeep.cursecdn.com/avatars/thumbnails/10/71/150/150/636339380148524382.png"])
+
+(defn <char-avatar>
+  [id image-url]
+  [:div.char-avatar
+   {:id id
+    :style {:background-image (str "url(" image-url ")")}
+    :draggable true}])
 
 (defn <nav>
   []
@@ -58,25 +69,28 @@
 (defn <map-preview>
   [w h active-position]
   (fn []
-    [:div.map-preview-wrapper
-     [:img.map-preview-img {:src "https://img00.deviantart.net/d36a/i/2015/115/3/0/abandoned_temple_of_blackfire_by_dlimedia-d4pponv.jpg"
-            :alt "Created by DLIMedia: https://www.deviantart.com/dlimedia/art/Abandoned-Temple-of-Blackfire-285053467"}]
-     [:table.map-preview-table
-      [:tbody.map-preview-tbody
-       (doall
-         (for [y (range @h)]
-           [:tr.map-preview-row {:key (str "m-prev-y" y)}
-            (doall
-              (for [x (range @w)]
-                [:td.map-preview-cell
-                 {:key (str "map-prev-yx-" y x)
-                  :on-click #(reset! active-position [x y])
-                  :class [(when (= [x y] @active-position)
-                           "map-cell__active")
-                          (when @highlight-overlay
-                            "map-cell__highlight")]}
-                 ;(str "(" x "," y ")")
-                 ]))]))]]]))
+    [:div 
+     [:div {:style {:height "100px"}}
+      (map-indexed (fn [i p]
+                     [<char-avatar> (str "player-" i) p])
+                   players) ]
+     [:div.map-preview-wrapper
+      [:img.map-preview-img {:src "https://img00.deviantart.net/d36a/i/2015/115/3/0/abandoned_temple_of_blackfire_by_dlimedia-d4pponv.jpg"
+                             :alt "Created by DLIMedia: https://www.deviantart.com/dlimedia/art/Abandoned-Temple-of-Blackfire-285053467"}]
+      [:table.map-preview-table
+       [:tbody.map-preview-tbody
+        (doall
+          (for [y (range @h)]
+            [:tr.map-preview-row {:key (str "m-prev-y" y)}
+             (doall
+               (for [x (range @w)]
+                 [:td.map-preview-cell
+                  {:key (str "map-prev-yx-" y x)
+                   :on-click #(reset! active-position [x y])
+                   :class [(when @highlight-overlay
+                             "map-cell__highlight")]}
+                  ;(str "(" x "," y ")")
+                  ]))]))]]]]))
 
 
 (defn <session-new>
