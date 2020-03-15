ff7c098eafa611cb80b57e2c579e05cd6e3c532d
Waldemar <waldemar.schwan@uswitch.com>
2020-03-15 11:55:01 +0000

    Update comp_lib.cljs

--- src/app/view/comp_lib.cljs
+++ src/app/view/comp_lib.cljs
@@ -1,7 +1,31 @@
-(ns app.view.comp-lib)
+(ns app.view.comp-lib
+  (:require
+    [app.browser :refer [log!]]
+    [app.view.components.mini-css
+     :refer [<button>
+             <link>
+             <accordion>
+             <accordion-section>]]))
 
 (defn <comp-lib-view>
   []
   [:<>
    [:h1 "Components library"]
-   [:p "something"]])
+
+   [:h2 "Link"]
+   [:span "This " [<link> {} "link"] " will take you somewhere." ]
+
+   [:h2 "Buttons"]
+   (for [c ["primary" "secondary" "tertiary" "inverse"]]
+     [<button>
+      {:color c
+       :on-click #(log! "button pressed" c)}
+      c])
+   
+   [:h2 "Accordion"]
+   [<accordion> {}
+    [<accordion-section>
+     {:checked true
+      :label "Nav"}
+     [:p [<link> {:href "/home"} "Home"]]
+     [:p [<link> {:href "/comp-lib"} "Comp lib"]]]]])
